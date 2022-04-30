7a4c2a5adf5d94c6433494e8364e7b321efa7d47
Kiyron Dyer-Allen <65980399+kda100@users.noreply.github.com>
2022-04-30 11:46:16 +0100

    Delete src directory

--- src/constants/storageNames.ts
+++
@@ -1,4 +0,0 @@
-export const clientsBucketName = "clients-zk1o3";
-export const coachesBucketName = "coaches-m6as4";
-export const coachChatsMediaBucketName = "coach-chats-media";
-export const profilePicFileName = "profilePic.png";
