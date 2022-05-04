3b4ae29ab0e10006d5f8bc3519c05ced76a67403
Kiyron Dyer-Allen <65980399+kda100@users.noreply.github.com>
2022-05-04 21:50:19 +0100

    Delete cloud_functions/lib directory

--- cloud_functions/lib/constants/documentNames.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.squatTrackerDoc = exports.deadliftTrackerDoc = exports.bodyWeightTrackerDoc = exports.benchPressTrackerDoc = void 0;
-exports.benchPressTrackerDoc = "benchPressTracker";
-exports.bodyWeightTrackerDoc = "bodyWeightTracker";
-exports.deadliftTrackerDoc = "deadliftWeightTracker";
-exports.squatTrackerDoc = "squatTracker";
-//# sourceMappingURL=documentNames.js.map
--- cloud_functions/lib/constants/strings.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.coachId = exports.clientId = exports.errorMessage = void 0;
-exports.errorMessage = "An error has occurred";
-exports.clientId = "clientId";
-exports.coachId = "coachId";
-//# sourceMappingURL=strings.js.map
